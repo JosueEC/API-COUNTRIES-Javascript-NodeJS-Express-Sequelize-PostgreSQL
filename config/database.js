197b426cedb788c4f029553a83e5cc7c335cfc29
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-03 14:51:54 -0600

    feat(index.js): :hammer: Added the synchronization configuration with the database and the clause logging to the database connection.

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@ const Sequelize = require('sequelize')
 
 const { DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, DB_HOST } = process.env
 
-const database = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`)
+const database = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+  logging: false
+})
 
 module.exports = database
--- index.js
+++ index.js
@@ -4,17 +4,12 @@ const database = require('./config/database')
 
 const { SERVER_PORT } = process.env
 
-async function testDatabaseConnection () {
-  try {
-    await database.authenticate()
-    console.log('Connection has been established successfully.')
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
-
-testDatabaseConnection()
-
-server.listen(SERVER_PORT, () => {
-  console.log(`server listening on port ${SERVER_PORT}`)
-})
+database.sync({ alter: true })
+  .then(() => {
+    server.listen(SERVER_PORT, () => {
+      console.log(`server listening on port ${SERVER_PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.error(error.message)
+  })
