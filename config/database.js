bccff39407ad9a3ce11f436d041583ebdd0df4d0
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-03 21:15:47 -0600

    feat(config/database): :hammer: Imported and executed the define function for the country model in the database module.

--- config/database.js
+++ config/database.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
 const Sequelize = require('sequelize')
 
+const defineCountry = require('../src/entities/country/models/country.model')
+
 const { DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, DB_HOST } = process.env
 
 const database = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false
 })
+
+defineCountry(database)
 
 module.exports = database
