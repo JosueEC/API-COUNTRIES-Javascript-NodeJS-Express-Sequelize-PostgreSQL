d9f0822ec127c87380f079b97d2d2961a2757e6a
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-06 10:13:31 -0600

    feat(src/server.js): :hammer: Added the cors, morgan and express.json middleware to the server.js module.

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { database } = require('./config/database')
 
 const { SERVER_PORT } = process.env
 
-const { getAllCountries } = require('./src/services/API Flagpedia/getAllCountries.service')
-const { bulkCreateCountries } = require('./src/services/Database/bulkCreateCountries.service')
+// const { getAllCountries } = require('./src/services/API Flagpedia/getAllCountries.service')
+// const { bulkCreateCountries } = require('./src/services/Database/bulkCreateCountries.service')
 
 database.sync({ alter: true })
   .then(() => {
-    server.listen(SERVER_PORT, async () => {
-      await loadDatabase()
+    server.listen(SERVER_PORT, () => {
+      // await loadDatabase()
       console.log(`server listening on port ${SERVER_PORT}`)
     })
   })
@@ -18,13 +18,13 @@ database.sync({ alter: true })
     console.error(error.message)
   })
 
-async function loadDatabase () {
-  try {
-    const countries = await getAllCountries()
-    await bulkCreateCountries(countries)
+// async function loadDatabase () {
+//   try {
+//     const countries = await getAllCountries()
+//     await bulkCreateCountries(countries)
 
-    console.info('database loaded')
-  } catch (error) {
-    return error.message
-  }
-}
+//     console.info('database loaded')
+//   } catch (error) {
+//     return error.message
+//   }
+// }
