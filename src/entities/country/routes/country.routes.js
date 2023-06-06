f829424c710b4c44958466f0e3329e58654a7fa3
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-06 12:33:43 -0600

    feat(entities/country/routes): :hammer: Created the endpoint /countries/:countryID.

--- src/entities/country/routes/country.routes.js
+++ src/entities/country/routes/country.routes.js
@@ -3,7 +3,16 @@ const countryController = require('../controllers/country.controller')
 
 const router = Router()
 
-// GET | /countries
-router.use('/', countryController.getCountries)
+//  GET | /countries
+router.get('/', countryController.getCountries)
+
+//  GET | /countries/:countryID
+router.get('/:idCountry', (req, res) => {
+  res.status(200).send({
+    code: 200,
+    status: 'FOUND',
+    data: 'country finded by ID'
+  })
+})
 
 module.exports = router
