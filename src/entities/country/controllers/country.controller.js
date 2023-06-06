c66c4c6d8d109bc0f281fc9f74f8011864a4a123
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-06 11:15:01 -0600

    feat(entities/country/controllers): :hammer: Linked the error and success helpers to the getCountries handler in the country controller.

--- src/entities/country/controllers/country.controller.js
+++ src/entities/country/controllers/country.controller.js
@@ -1,18 +1,13 @@
+const { HTTP_NOT_FOUND, HTTP_FOUND } = require('../../../constants/httpCodes.constants')
+const { httpError } = require('../../../helpers/httpError.helper')
+const { httpSuccess } = require('../../../helpers/httpSuccess.helper')
+
 // TODO: Create the service for this handler
-// TODO: Add the successHelper and errorHelper
 const getCountries = (req, res) => {
   try {
-    res.status(200).send({
-      code: 200,
-      status: 'FOUND',
-      data: 'handler GET /countries'
-    })
+    httpSuccess(res, 'handler with helper GET /countries', HTTP_FOUND)
   } catch (error) {
-    res.status(400).send({
-      code: 400,
-      status: 'NOT FOUND',
-      error: error.message
-    })
+    httpError(res, error, HTTP_NOT_FOUND)
   }
 }
 
