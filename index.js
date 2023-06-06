b8de9e68763d2ec5df11e93a7d222da5ad12dfb5
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-05 21:05:24 -0600

    refactor(index.js): :recycle: Separated the getAllCountries function from the database.sync function.

--- index.js
+++ index.js
@@ -9,10 +9,18 @@ const { getAllCountries } = require('./src/services/API Flagpedia/getAllCountrie
 database.sync({ alter: true })
   .then(() => {
     server.listen(SERVER_PORT, async () => {
-      await getAllCountries()
+      await loadDatabase()
       console.log(`server listening on port ${SERVER_PORT}`)
     })
   })
   .catch((error) => {
     console.error(error.message)
   })
+
+async function loadDatabase () {
+  try {
+    await getAllCountries()
+  } catch (error) {
+    return error.message
+  }
+}
