e5a486d4e7c8698fd76a044ca2b30f2fc57d9255
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-06 13:29:22 -0600

    feat(src/helpers): :hammer: Created the validate.helper to validate the request values.

---
+++ src/helpers/validate.helper.js
@@ -0,0 +1,17 @@
+const { HTTP_BAD_REQUEST } = require('../constants/httpCodes.constants')
+const { validationResult } = require('express-validator')
+
+const validateResult = (req, res, next) => {
+  try {
+    validationResult(req).throw()
+    return next()
+  } catch (error) {
+    res.status(HTTP_BAD_REQUEST).send({
+      code: HTTP_BAD_REQUEST.CODE,
+      status: HTTP_BAD_REQUEST.STATUS,
+      error: error.array()
+    })
+  }
+}
+
+module.exports = { validateResult }
