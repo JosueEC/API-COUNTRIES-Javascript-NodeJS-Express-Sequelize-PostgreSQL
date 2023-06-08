696a97fd4424ee3204f268dc481f77b52c5177ef
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-08 16:38:23 -0600

    feat(index.js): :sparkles: The API can check if the database is already loaded or not.

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const { database } = require('./config/database')
 
 const { SERVER_PORT } = process.env
 
-// const { getAllCountries } = require('./src/services/API Flagpedia/getAllCountries.service')
-// const { bulkCreateCountries } = require('./src/services/Database/bulkCreateCountries.service')
+const { findAllCountries } = require('./src/entities/country/services/findAllCountries.service')
+const { getAllCountries } = require('./src/services/API Flagpedia/getAllCountries.service')
+const { bulkCreateCountries } = require('./src/services/Database/bulkCreateCountries.service')
 
 database.sync({ alter: true })
   .then(() => {
-    server.listen(SERVER_PORT, () => {
-      // await loadDatabase()
+    server.listen(SERVER_PORT, async () => {
+      await loadDatabase()
       console.log(`server listening on port ${SERVER_PORT}`)
     })
   })
@@ -18,13 +19,18 @@ database.sync({ alter: true })
     console.error(error.message)
   })
 
-// async function loadDatabase () {
-//   try {
-//     const countries = await getAllCountries()
-//     await bulkCreateCountries(countries)
+async function loadDatabase () {
+  try {
+    const countriesDatabase = await findAllCountries()
 
-//     console.info('database loaded')
-//   } catch (error) {
-//     return error.message
-//   }
-// }
+    if (countriesDatabase.length === 0) {
+      const countries = await getAllCountries()
+      await bulkCreateCountries(countries)
+      console.info('database loaded')
+    } else {
+      console.info('the database is already loaded')
+    }
+  } catch (error) {
+    console.info(error.message)
+  }
+}
