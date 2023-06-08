fdd48a766cbfec5a4bd3364ca3dc095b156fa08f
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-07 20:02:12 -0600

    feat(entities/activity/validators): :sparkles: The API can validate the data to create a new activity and handle the errors with this data.

--- src/entities/activity/routes/activity.routes.js
+++ src/entities/activity/routes/activity.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 
 const activityController = require('../controllers/activity.controller')
+const { validateCreateActivity } = require('../validators/validateCreateActivity.validator')
 
 const router = Router()
 
@@ -8,6 +9,6 @@ const router = Router()
 router.get('/', activityController.getActivities)
 
 //  POST | /activities
-router.post('/', activityController.postActivity)
+router.post('/', validateCreateActivity, activityController.postActivity)
 
 module.exports = router
---
+++ src/entities/activity/validators/validateCreateActivity.validator.js
@@ -0,0 +1,46 @@
+const { body } = require('express-validator')
+const { validateResult } = require('../../../helpers/validate.helper')
+
+const validateCreateActivity = [
+  body('name')
+    .exists()
+    .notEmpty(),
+  body('difficulty')
+    .exists()
+    .notEmpty()
+    .isNumeric()
+    .isInt()
+    .isIn([1, 2, 3, 4, 5]),
+  body('duration')
+    .exists()
+    .notEmpty()
+    .isNumeric()
+    .isInt()
+    .custom((value) => {
+      if (value >= 1 && value <= 10) return true
+      throw new Error('invalid activity duration')
+    }),
+  body('season')
+    .exists()
+    .notEmpty()
+    .isLowercase()
+    .isIn(['verano', 'otoño', 'invierno', 'primavera']),
+  body('countries')
+    .exists()
+    .notEmpty()
+    .isArray()
+    .custom((value) => {
+      const regexCountryID = /^[A-Z]{3}$/
+      const validIDS = value.every((idCountry) => {
+        return regexCountryID.test(idCountry)
+      })
+
+      if (!validIDS) throw new Error('invalid country ID for the foreign key')
+      return true
+    }),
+  (req, res, next) => {
+    validateResult(req, res, next)
+  }
+]
+
+module.exports = { validateCreateActivity }
